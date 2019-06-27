923c34ac00ee14b46eb50017d4ed3c8a200c7fdb
Sean Moss <moss.seank@gmail.com>
2019-06-26 19:45:53 -0600

    Updated uniform reflection and added uniform block reflection

--- hlsv/reflect/writer.cpp
+++ hlsv/reflect/writer.cpp
@@ -11,6 +11,8 @@
 #include "io.hpp"
 #include <fstream>
 
+#define WRITE_LE16(val) uint8((val)&0xFF) << uint8(((val)>>8)&0xFF)
+
 
 namespace hlsv
 {
@@ -97,14 +99,37 @@ bool ReflWriter::WriteText(const string& path, const ReflectionInfo& refl, strin
 		 << "--------" << std::endl;
 	if (refl.uniforms.size() > 0) {
 		file << pad("Name", 16) << ' ' << pad("Type", 16) << ' ' << pad("Type Arg.", 12) << ' ' << pad("Array", 10) << ' '
-			 << pad("Set", 8) << ' ' << pad("Binding", 8) << std::endl; // TODO: The offset and block, once implemented
+			 << pad("Set", 8) << ' ' << pad("Binding", 8) << ' ' << pad("Block", 8) << ' ' << pad("Offset", 8) << ' '
+			 << pad("Count", 8) << ' ' << pad("Size", 8) << std::endl;
 		for (const auto& uni : refl.uniforms) {
 			file << pad(uni.name, 16) << ' ' << pad(uni.type.get_type_str(), 16) << ' ';
 			if (uni.type.is_image_type()) file << pad(HLSVType::GetTypeStr(uni.type.extra.image_format), 12);
 			else if (uni.type == HLSVType::SubpassInput) file << padf("%u", 12, (uint32)uni.type.extra.subpass_input_index);
 			else file << pad("", 12);
-			file << ' ' << pad(uni.type.is_array ? "Yes" : "No", 10) << ' ' << padf("%u", 8, (uint32)uni.set) << ' ' 
-				 << padf("%u", 8, (uint32)uni.binding) << std::endl;
+			file << ' ' << pad(uni.type.is_array ? "Yes" : "No", 10) << ' ' << padf("%u", 8, (uint32)uni.set) << ' '
+				 << padf("%u", 8, (uint32)uni.binding);
+			if (uni.type.is_value_type()) {
+				file << ' ' << padf("%u", 8, (uint32)uni.block.index) << ' ' << padf("%u", 8, (uint32)uni.block.offset)
+					 << ' ' << padf("%u", 8, (uint32)uni.type.count) << ' ' << padf("%u", 8, (uint32)uni.block.size)
+					 << std::endl;
+			}
+			else file << std::endl;
+		}
+		file << std::endl;
+	}
+	else
+		file << "None" << std::endl << std::endl;
+
+	// Uniform blocks
+	file << "Uniform Blocks" << std::endl
+		 << "--------------" << std::endl;
+	if (refl.blocks.size() > 0) {
+		file << pad("Set", 8) << ' ' << pad("Binding", 8) << ' ' << pad("Members", 8) << ' ' << pad("Packed", 8) << ' ' 
+			 << pad("Size", 8) << std::endl;
+		for (const auto& bl : refl.blocks) {
+			file << padf("%u", 8, (uint32)bl.set) << ' ' << padf("%u", 8, (uint32)bl.binding) << ' '
+				 << padf("%u", 8, (uint32)bl.members.size()) << ' ' << pad(bl.packed ? "Yes" : "No", 8) << ' '
+				 << padf("%u", 8, (uint32)bl.size) << std::endl;
 		}
 		file << std::endl;
 	}
@@ -155,10 +180,22 @@ bool ReflWriter::WriteBinary(const string& path, const ReflectionInfo& refl, str
 	// Write uniforms
 	file << (uint8)refl.uniforms.size();
 	if (refl.uniforms.size() > 0) {
-		// TODO: The offset and block, once implemented
 		for (const auto& uni : refl.uniforms) {
 			write_str(file, uni.name) << (uint8)uni.type.type << (uint8)uni.type.extra.image_format // Writes whatever extra information there is
-				<< (uint8)(uni.type.is_array ? uni.type.count : 0) << uni.set << uni.binding;
+				<< (uint8)(uni.type.is_array ? uni.type.count : 0) << uni.set << uni.binding << uni.block.index
+				<< WRITE_LE16(uni.block.offset) << WRITE_LE16(uni.block.size);
+		}
+	}
+
+	// Write uniform blocks
+	file << (uint8)refl.blocks.size();
+	if (refl.blocks.size() > 0) {
+		for (const auto& bl : refl.blocks) {
+			file << bl.set << bl.binding << WRITE_LE16(bl.size) << (uint8)(bl.packed ? 1 : 0);
+			file << (uint8)bl.members.size();
+			for (auto mi : bl.members) {
+				file << mi;
+			}
 		}
 	}
 
